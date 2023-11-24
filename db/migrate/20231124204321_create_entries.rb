7337e6474e19835982a43c169e570676c51182e5
hugo <hugo@hugos-MacBook-Air.local>
2023-11-24 14:57:38 -0600

    scaffold entry

---
+++ db/migrate/20231124204321_create_entries.rb
@@ -0,0 +1,14 @@
+class CreateEntries < ActiveRecord::Migration[7.1]
+  def change
+    create_table :entries do |t|
+      t.string :meal_type
+      t.integer :calories
+      t.integer :proteins
+      t.string :carbohydrates
+      t.string :integer
+      t.integer :fats
+
+      t.timestamps
+    end
+  end
+end
